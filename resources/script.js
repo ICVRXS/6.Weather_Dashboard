b394598e06910f9cbad1d043d6043770e5bc4a81
ICVRXS <calebhay@rocketmail.com>
2020-05-06 15:09:07 -0700

    started coding out logic

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,32 @@
 $("#test").on("click", function(){
     console.log("Hello, World!");
 });
+
+var citiesStore = localStorage.getItem("weatherByCity");
+
+if (!citiesStore){
+    citiesStore = {};
+}
+
+function renderCityData(cityName){
+    
+}
+
+function getCityData(cityName){
+    
+}
+
+function fetchCityData(cityName){
+    
+}
+
+var queryUrl = "api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey;
+var apiKey = "9fc203891252cc2336158c490f709da2";
+var cityName = "Big Bear Lake";
+
+$.ajax({
+    url: queryUrl,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+});
