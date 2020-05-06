8cd9a8ef14a972b801bb6d429c9b354ed52c25d2
ICVRXS <calebhay@rocketmail.com>
2020-05-06 16:34:57 -0700

    fixed syntax errors, started testing functionality

--- resources/script.js
+++ resources/script.js
@@ -1,32 +1,45 @@
-$("#test").on("click", function(){
-    console.log("Hello, World!");
-});
-
-var citiesStore = localStorage.getItem("weatherByCity");
+var citiesStore = JSON.parse(localStorage.getItem("weatherByCity"));
 
 if (!citiesStore){
     citiesStore = {};
 }
 
 function renderCityData(cityName){
-    
+    console.log(cityName);
+    var cityData = getCityData(cityName);
+    if(!cityData){
+        console.log("no data in local storage");
+        return;
+    }
+    console.log("got data from local storage");
+    var renderCityName = $("<h1>"+cityData.name+"</h1>");
+    $(".city-name").append(renderCityName);
 }
 
 function getCityData(cityName){
-    
+    if(citiesStore[cityName]){
+        return citiesStore[cityName];
+    }else{
+        fetchCityData(cityName);
+        return false;
+    }
 }
 
 function fetchCityData(cityName){
-    
+    var queryUrl = "api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey;
+    var apiKey = "9fc203891252cc2336158c490f709da2";
+    var cityName = "";
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET",
+    }).then(function(response){
+        console.log(response);
+        citiesStore;
+        renderCityData(cityName);
+    });
 }
 
-var queryUrl = "api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey;
-var apiKey = "9fc203891252cc2336158c490f709da2";
-var cityName = "Big Bear Lake";
 
-$.ajax({
-    url: queryUrl,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
-});
+
+renderCityData("Big Bear Lake, CA");
