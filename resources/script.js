d2367735a8b8256e600d0687860cfec1a2ef38de
ICVRXS <calebhay@rocketmail.com>
2020-05-06 20:05:47 -0700

    fixed syntax on ajax url, made search area and added function to capture searches and run them through render city function

--- resources/script.js
+++ resources/script.js
@@ -1,45 +1,63 @@
 var citiesStore = JSON.parse(localStorage.getItem("weatherByCity"));
 
-if (!citiesStore){
+$("#search-button").on("click", function(){
+    var citySearch = $("#search-input").val();
+    $("#search-input").val("");
+    renderCityData(citySearch);
+})
+
+$("#search-input").on("keypress", function(e) {
+    if (e.keyCode === 13){
+        var citySearch = $("#search-input").val();
+        $("#search-input").val("");
+        renderCityData(citySearch);
+    }
+})
+
+if (!citiesStore) {
     citiesStore = {};
 }
 
-function renderCityData(cityName){
+function renderCityData(cityName) {
     console.log(cityName);
     var cityData = getCityData(cityName);
-    if(!cityData){
+    if (!cityData) {
         console.log("no data in local storage");
         return;
     }
     console.log("got data from local storage");
-    var renderCityName = $("<h1>"+cityData.name+"</h1>");
+    var renderCityName = $("<h1>" + cityData.name + "</h1>");
     $(".city-name").append(renderCityName);
 }
 
-function getCityData(cityName){
-    if(citiesStore[cityName]){
+function getCityData(cityName) {
+    if (citiesStore[cityName]) {
         return citiesStore[cityName];
-    }else{
+    } else {
         fetchCityData(cityName);
         return false;
     }
 }
 
-function fetchCityData(cityName){
-    var queryUrl = "api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey;
-    var apiKey = "9fc203891252cc2336158c490f709da2";
-    var cityName = "";
+function fetchCityData(cityName) {
+    var queryParams = $.param({
+        q: cityName,
+        appid: "9fc203891252cc2336158c490f709da2"
+    });
+    var queryUrl = "http://api.openweathermap.org/data/2.5/weather?" + queryParams;
+
+    console.log(queryUrl);
 
     $.ajax({
         url: queryUrl,
         method: "GET",
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
-        citiesStore;
+        citiesStore[cityName] = {
+            name: response.name
+        };
         renderCityData(cityName);
     });
 }
 
-
-
-renderCityData("Big Bear Lake, CA");
+// renderCityData();
